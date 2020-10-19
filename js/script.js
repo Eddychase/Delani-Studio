99d7846d4f12bdd5ac3fbde5969364ef892c785c
Eddy Chase <71647269+Eddychase@users.noreply.github.com>
2020-10-19 12:07:09 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,30 +1,22 @@
-(document).ready(function(){
+$(document).ready(function() {
 
-  $('.submit').click(function () {
-    var Name = $('#mce-NAME').val();
-    var Email = $('#mce-EMAIL').val();
-    var Message = $('#mce-MESSAGE');
-    
-    if (Name == '' || Email == '' || Message == '') {
-        alert('Please Fill in the form correctly!');
-    } else {
-        alert(' Greetings ' + Name + ' We have received your message. Thank you.');
-    }
+  $(".Hover").hover(function(){
+    $(this).animate({opacity:'1'});
+  })
+ 
+
+  $("#design").click(function(){
+    $("#showDesign").toggle();
+    $(".hideDesign").toggle();
+  })
+
+  $("#development").click(function(){
+    $("#development-showing").toggle();
+    $(".hideDevelopment").toggle();
+  })
+
+  $("#productManagement").click(function(){
+    $("#showProductmanagement").toggle();
+    $(".hideProductmanagement").toggle();
+  })
 });
-
-  
-$("#design").click(function(){
-  $("#design-showing").toggle();
-  $(".hide-design-image").toggle();
-})
-
-$("#development").click(function(){
-  $("#development-showing").toggle();
-  $(".hide-development-image").toggle();
-})
-
-$("#productManagement").click(function(){
-  $("#product-management-showing").toggle();
-  $(".hide-product-management").toggle();
-})
-})
